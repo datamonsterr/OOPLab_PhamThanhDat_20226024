a929b672a58f3948a91e5a6e6c1090c213a0a890
datamonsterr <phamthanhdat17092004@gmail.com>
2024-03-27 22:15:38 +0700

    Complete 7 to 12 assignments

---
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,53 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    // constructor
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+    }
+
+    // generate getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+}
