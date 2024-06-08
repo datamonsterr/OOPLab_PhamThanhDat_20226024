bd53ab9b9cf9b071e405b3baf28ee204890bdd70
datamonsterr <phamthanhdat17092004@gmail.com>
2024-06-08 19:41:41 +0700

    add .gitignore file

---
+++ AimsProject/src/hust/soict/globalict/aims/screen/customer/Customer.java
@@ -0,0 +1,14 @@
+package hust.soict.globalict.aims.screen.customer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CustomerScreen extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
